61871e202174e2b311a10be7eeec7a361a54231c
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-02-05 01:52:04 +0530

    basics

--- baby.hs
+++ baby.hs
@@ -1,1 +1,16 @@
 
+doubleMe x = x + x
+
+doubleUs x y=  doubleMe x  + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else doubleMe
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+multiplyBy5 range= [ x * 5 | x <- [1..range]]
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+len xs = sum[1 | _ <- xs]
+
+removeNonUpperCase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
