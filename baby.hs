bf3239e1912d78ade33eadec4be062ecb5919dc8
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-02-05 11:06:04 +0530

    implement nested list comprehension

--- baby.hs
+++ baby.hs
@@ -14,3 +14,5 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 len xs = sum[1 | _ <- xs]
 
 removeNonUpperCase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+
+nestedListComprehension xxs = [[ x | x <- xs, even x ] | xs <- xxs]
