ff176406c487921b1a12e47cb6bbc8df6e021428
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-02-22 22:48:06 +0530

    implement TailRecursionAndHigherOrderFunctionsII

---
+++ TailRecursionAndHigherOrderFunctionsII.hs
@@ -0,0 +1,21 @@
+isPrime :: Int -> Bool
+isPrime n =  not $ any(\x ->  n `mod` x == 0) [2..(n `div` 2)]
+
+nPrimes ::  Int -> [Int]
+nPrimes n =  take n $ filter isPrime  [2..]
+
+dupNTimes :: Int -> [a] -> [a]
+dupNTimes n xs = concat $ [ replicate n x | x <-xs]
+
+span' :: (Eq a,Ord a) => (a -> Bool) -> [a] -> ([a],[a])
+span' f xs =
+  let takeList = [ x | x <- xs , f x ]
+      dropList = [ x | x <- xs , not $ f x  ]
+  in  (takeList,dropList)
+
+removeDuplicatesFromSortedCollection :: (Eq a) => [a] -> [a]
+removeDuplicatesFromSortedCollection [] = []
+removeDuplicatesFromSortedCollection [x] = [x]
+removeDuplicatesFromSortedCollection (x:xs)
+     | x == head xs = removeDuplicatesFromSortedCollection xs
+     | otherwise = x:removeDuplicatesFromSortedCollection xs
