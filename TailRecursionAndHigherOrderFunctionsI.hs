f1f05684a33cbb94f597c7d3b72b705ae9fcd6d0
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-02-23 18:47:15 +0530

    implement map using tail recursion;implement reverse list with foldl and flip;implement power with product and replicate

--- TailRecursionAndHigherOrderFunctionsI.hs
+++ TailRecursionAndHigherOrderFunctionsI.hs
@@ -1,13 +1,19 @@
 
 sumUpto :: Int -> Int
-sumUpto n = sum $ [1..n]
+sumUpto n = sum [1..n]
 
 map1 :: (a -> b) -> [a] -> [b]
-map1 f xs = [ f e | e <- xs ]
+map1 f  xs = [ f e | e <- xs ]
 
 map2 :: (a -> b) -> [a] -> [b]
 map2 _ [] = []
 map2 f (x:xs) = f x : map2 f xs
+
+map3 :: (a -> b) -> [a] -> [b]
+map3 _ [] = []
+map3 f xs = loop f xs []
+    where  loop f (x:[]) acc =  reverse (f x : acc)
+           loop f (x:xs) acc = loop f xs (f x : acc)
 
 filter1 :: (a -> Bool) -> [a] -> [a]
 filter1 f xs = [ x | x <- xs, f x]
@@ -24,6 +30,7 @@ exists d (x:xs)
    | d == x = True
    | otherwise = exists d xs
 
+
 forAll :: Ord a => ( a -> Bool) -> [a] -> Bool
 forAll _ [] = False
 forAll f [x] = f x
@@ -36,7 +43,7 @@ reverseList [] = []
 reverseList (x:xs) =  reverseList xs ++ [x]
 
 reverseListUsingFoldLeft :: [a] -> [a]
-reverseListUsingFoldLeft xs = foldl (\acc e -> e : acc) [] xs
+reverseListUsingFoldLeft xs = foldl (flip(:)) [] xs
 
 pow :: Int -> Int -> Int
-pow x y = foldl (\acc e -> acc * x ) 1 [1..y]
+pow x y = product $ replicate y x
